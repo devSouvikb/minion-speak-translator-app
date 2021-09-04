88bea0d6a2d4e06ddb8aca1702d1d4f214c58364
Souvik <souvikbiswas792@gmail.com>
2021-09-04 20:45:20 +0530

    Update app.js : added click event to button

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ let btnTranslate = document.querySelector("#btn-translate");
 let txtOutput = document.querySelector("#output");
 
 let serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+btnTranslate.addEventListener("click", clickEventHandler);
