1a13fb7d2094f2f9818ccc1586ff8769511bb497
Souvik <souvikbiswas792@gmail.com>
2021-09-04 20:41:29 +0530

    Update app.js : linked api server

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 let txtInput = document.querySelector("#txt-input");
 let btnTranslate = document.querySelector("#btn-translate");
 let txtOutput = document.querySelector("#output");
+
+let serverURL = "https://api.funtranslations.com/translate/minion.json";
