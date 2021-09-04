c3861798352a8894afbe4bd6d2fcd8cc0ad612a1
Souvik <souvikbiswas792@gmail.com>
2021-09-04 20:52:18 +0530

    Update : added error handler

--- app.js
+++ app.js
@@ -4,4 +4,13 @@ let txtOutput = document.querySelector("#output");
 
 let serverURL = "https://api.funtranslations.com/translate/minion.json";
 
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured" + error);
+  alert("Something is wrong with server please try after sometime.");
+}
+
 btnTranslate.addEventListener("click", clickEventHandler);
